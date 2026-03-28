d103f1099490d9e21af598bf8db97af14e123128
Tarunk8- <mrtarunkumar303@gmail.com>
2026-03-28 18:08:04 +0530

    sg-grroup

--- terraform_code/V1-EC2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-region = "us-east-1"  
-}
-
-resource "aws_instance" "demo-server" {
-    ami = "ami-0c3389a4fa5bddaad"
-    instance_type = "t3.small"
-    key_name = "Devops-key"
-}
