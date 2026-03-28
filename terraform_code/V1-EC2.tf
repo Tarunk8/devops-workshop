a71748da7798db69fd3d34e9e7438ffce453c1d9
Tarunk8- <mrtarunkumar303@gmail.com>
2026-03-28 17:02:09 +0530

    v1-ec2.tf

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+region = "us-east-1"  
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
-    instance_type = "t2.micro"
-    key_name = "dpp"
+    ami = "ami-0c3389a4fa5bddaad"
+    instance_type = "t3.small"
+    key_name = "Devops-key"
 }
